dbf33b1173c61f88c4893a1442148574736ff874
John Weland <jweland@icsolutions.com>
2020-11-19 09:51:29 -0600

    fleshed out basic sw and manifest

--- js/application.js
+++ js/application.js
@@ -63,3 +63,12 @@ const toggleTheme = () => {
 }
 
 toggle.addEventListener("click", toggleTheme);
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("../sw.js")
+        .catch((err) => console.error(`Service Worker: Error: ${err}`));
+    });
+}
