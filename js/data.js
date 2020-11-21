a9365797c7d93a0a623a4ac93dd8300befe8b452
John Weland <john.weland@gmail.com>
2020-11-20 21:42:26 -0600

    finished data object

--- js/data.js
+++ js/data.js
@@ -19,6 +19,96 @@ let data = [
         desc: "Herters 5.56 NATO 55 Grain FMJ",
         count: "300 round can loose",
         price: "$240"
-    }
+    }, {
+        img: "img/556_Federal_XM193BK.jpg",
+        desc: "Federal American Eagle 5.56mm NATO XM193 55 Grain FMJ",
+        count: "1000 round box loose",
+        price: "$800"
+    }, {
+        img: "img/223_Federal_AE223.jpg",
+        desc: "Federal American Eagle .223 Remington 55 Grain FMJ-BT",
+        count: "20 round box",
+        price: "$19"
+    }, {
+        img: "img/223_Fiocchi_223a.jpg",
+        desc: "Fiocchi .223 Remington 55 Grain FMJ",
+        count: "50 round box",
+        price: "$45"
+    }, {
+        img: "img/223_PMC_223A.jpg",
+        desc: "PMC Bronze .223 Remington 55 Grain FMJ-BT",
+        count: "20 round box",
+        price: "$19"
+    }, {
+        img: "img/22LR_Federal_AM22.jpg",
+        desc: "Federal Premium .22 LR Rimfire 40 Grain",
+        count: "325 round box",
+        price: "$45"
+    }, {
+        img: "img/22LR_Remington_TB-22B.jpg",
+        desc: "Remington Thunderbolt .22 LR Rimfire 40 Grain",
+        count: "500 round box",
+        price: "$60"
+    }, {
+        img: "img/45_Magtech 45A.jpg",
+        desc: "Magtech .45ACP 230 Grain FMJ",
+        count: "50 round box",
+        price: "$35"
+    }, {
+        img: "img/45_Winchester_USA45W.jpg",
+        desc: "Winchester USA .45ACP 230 Grain FMJ",
+        count: "50 round box",
+        price: "$35"
+    }, {
+        img: "img/45_Winchester_USA45W.jpg",
+        desc: "Winchester USA .45ACP 230 Grain FMJ",
+        count: "200 round box",
+        price: "$140"
+    }, {
+        img: "img/40_Winchester_SXZ40T.jpg",
+        desc: "Winchester SXZ .40 S&W 180 Grain FMJ",
+        count: "50 round box",
+        price: "$45"
+    }, {
+        img: "img/40_Winchester_usa40sw.jpg",
+        desc: "Winchester USA .40 S&W 165 Grain FMJ",
+        count: "350 round box",
+        price: "$245"
+    }, {
+        img: "img/40_Loose01.jpg",
+        desc: "Mixed 40 S&W S&B JHP",
+        count: "100 loose bag",
+        price: "$75"
+    }, {
+        img: "img/9mm_Magtech_9A.jpg",
+        desc: "Magtech 40 S&W 180 Grain FMJ",
+        count: "50 round box",
+        price: "$35"
+    }, {
+        img: "img/40_Defender.jpg",
+        desc: "Defender 40 S&W 180 Grain FMJ",
+        count: "50 round box",
+        price: "$35"
+    }, {
+        img: "img/9makarov_Fiocchi.jpg",
+        desc: "Fiocchi 9mmx18mm Makarov 95 Grain FMJ",
+        count: "50 round box",
+        price: "$30"
+    }, {
+        img: "img/300_PPU_PP300BH.jpg",
+        desc: "PPU Prvi Partizan 300 AAC Blackout 125 Grain HPBT",
+        count: "20 round box",
+        price: "$35"
+    }, {
+        img: "img/magazine_Magpul_223.jpg",
+        desc: "Magpul PMAG AR-15 5.56/.223 30/round Magazine Gen M3",
+        count: "Single Unit",
+        price: "$15"
+    }, {
+        img: "img/Magazine_Brownells_AR15.jpg",
+        desc: "Brownells AR-15 Mil-Spec USGI 5.56/.223 30/round Magazine Magpul Follower & Stainless Steel Spring",
+        count: "Single Unit",
+        price: "$15"
+    },
 ];
 export default data;
