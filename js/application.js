010bd265a991e4796566576bfb74466fade35218
John Weland <john.weland@gmail.com>
2020-11-20 21:14:04 -0600

    testing templates and modules

--- js/application.js
+++ js/application.js
@@ -1,4 +1,6 @@
 const toggle = document.getElementById('themeToggle');
+const listings = document.getElementById('producttable');
+import data from './data.js';
 
 const toggleTheme = () => {
     const lightBackgrounds = document.querySelectorAll('.bg-light');
@@ -9,11 +11,13 @@ const toggleTheme = () => {
     const navDark = document.querySelector('.navbar-dark');
     const borderLight = document.querySelectorAll('.border-light');
     const borderDark = document.querySelectorAll('.border-dark');
+    const lightTables = document.querySelectorAll('.table-light');
+    const darkTables = document.querySelectorAll('.table-dark');
 
     if (lightBackgrounds.length) {
         lightBackgrounds.forEach(background => {
-            background.classList.remove('bg-light');
-            background.classList.add('bg-dark');            
+            background.classList.remove('table-light');
+            background.classList.add('table-dark');            
         });
     }
 
@@ -60,9 +64,48 @@ const toggleTheme = () => {
             border.classList.add('border-light');   
         });             
     }
+
+    if (lightTables.length) {
+        lightTables.forEach(table => {
+            table.classList.remove('table-light');
+            table.classList.add('table-dark');   
+        });          
+    }
+
+    if (darkTables.length) {
+        darkTables.forEach(table => {
+            table.classList.remove('table-dark');
+            table.classList.add('table-light');   
+        });             
+    }
 }
 
 toggle.addEventListener("click", toggleTheme);
+
+if ('content' in document.createElement('template')) {
+
+    // Instantiate the table with the existing HTML tbody
+    // and the row with the template
+    const tbody = document.querySelector("tbody");
+    const template = document.querySelector('#productrow');
+    if (listings) {
+        data.forEach(item => {
+            let clone = template.content.cloneNode(true);
+            let td = clone.querySelectorAll("td");
+            let img = document.createElement("img");
+            img.src = item.img;
+            td[0].appendChild(img);
+            td[1].textContent = item.desc;
+            td[2].textContent = item.count;
+            td[3].textContent = item.price;
+            tbody.appendChild(clone);
+        });
+    }
+
+} else {
+  // Find another way to add the rows to the table because 
+  // the HTML template element is not supported.
+}
 
 
 if ("serviceWorker" in navigator) {
@@ -72,3 +115,4 @@ if ("serviceWorker" in navigator) {
         .catch((err) => console.error(`Service Worker: Error: ${err}`));
     });
 }
+
---
+++ js/data.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+        img: "img/9makarov_Fiocchi.jpg",
+        desc: "Speer Lawman 9mm Luger 147 Grain TMJ",
+        count: "50 round box",
+        price: "$40"
+    }, {
+        img: "img/9mm_Maxxtech_PTGB9MMB.jpg",
+        desc: "MAXXTECH 9mm Luger 115 Grain FMJ",
+        count: "50 round box",
+        price: "$40"
+    }, {
+        img: "img/9mm_Fiocchi_9AP.jpg",
+        desc: "Fiocchi 9mm Luger 115 Grain FMJ",
+        count: "50 round box",
+        price: "$35"
+    }, {
+        img: "img/556_herters_300.jpg",
+        desc: "Herters 5.56 NATO 55 Grain FMJ",
+        count: "300 round can loose",
+        price: "$240"
+    }
+];
+export default data;
